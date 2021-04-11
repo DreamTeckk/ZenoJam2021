e4f7a295a740033967dd9330a5c3d8468cb162be
Nathan <nathan.arago@gmail.com>
2021-04-11 23:18:53 +0200

    Started map design

--- Game/src/Scenes/MainScene.gd
+++ Game/src/Scenes/MainScene.gd
@@ -3,13 +3,13 @@ extends Node
 onready var ground := $Map/Ground
 onready var pathfinding := $Map/Pathfinding
 onready var player := $Map/YSort/Player
-onready var mission_manager := $HUD/Interface/Missions
+onready var mission_manager := $GUI/HUD/Interface/Missions
 onready var objective_manager := $Map/Objectives
 
-var difficulty = 1
+var difficulty = 5
 
 func _ready() -> void:
-	$HUD/Interface/DifficultyMetter/Level.text = str(difficulty)
+	$GUI/HUD/Interface/DifficultyMetter/Level.text = str(difficulty)
 	pathfinding.create_navigation_map(ground)
 	print_debug($Map/YSort/Ennemies.get_children().size())
 		
@@ -22,10 +22,10 @@ func _ready() -> void:
 		ennemy.setup(pathfinding, player, difficulty)
 
 func _process(delta: float) -> void:
-	$HUD/Interface/DifficultyMetter.value = 100 - ($DifficultyIncreaser.time_left / $DifficultyIncreaser.wait_time) * 100
+	$GUI/HUD/Interface/DifficultyMetter.value = 100 - ($DifficultyIncreaser.time_left / $DifficultyIncreaser.wait_time) * 100
 	
-	$HUD/Interface/VBoxContainer/LifeBar.value = (player.health / player.max_health) * 100
-	$HUD/Interface/VBoxContainer/LifeBar/Label.text = str(player.health) +  " / " + str(player.max_health)
+	$GUI/HUD/Interface/VBoxContainer/LifeBar.value = (player.health / player.max_health) * 100
+	$GUI/HUD/Interface/VBoxContainer/LifeBar/Label.text = str(player.health) +  " / " + str(player.max_health)
 
 func _on_Player_interact(objective_id: int) -> void:
 	mission_manager.check_mission_completion_with_objective_id(objective_id)
@@ -34,7 +34,7 @@ func _on_Player_interact(objective_id: int) -> void:
 func _on_DifficultyIncreaser_timeout() -> void:
 	# Increase level of dificulty
 	difficulty += 1
-	$HUD/Interface/DifficultyMetter/Level.text = str(difficulty)
+	$GUI/HUD/Interface/DifficultyMetter/Level.text = str(difficulty)
 	for ennemy in $Map/YSort/Ennemies.get_children():
 		ennemy.upgrade(difficulty)
 	pass
